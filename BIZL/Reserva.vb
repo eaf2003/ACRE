da8c6c38daf8fc4f7d171c9c1b6d43d4111d6cb5
pepe <ernestfarias@gmail.com>
2018-10-11 22:13:36 +0200

    s

---
+++ BIZL/Reserva.vb
@@ -0,0 +1,298 @@
+﻿Imports Microsoft.SqlServer
+Imports ACRE.Datos
+Imports System.Data
+
+Public Class Reserva
+    Inherits Reservas
+    Public nombre As String
+    'NO LLAMAR DIRECTO A ESA CLASSE, HACERLO ATRAVEZ DE RESERVAS CON S, QUE ES UNA COLLECCION DE RESERVA,S
+    '  Private oCfg As New Datos.Configuracion
+    '   Private oBase As New Datos.BaseSQL
+    '    Private odtReserva As New DataTable
+    Private vIDreserva As Integer
+    Private vMesa As Integer
+    Private vCantPax As Integer
+    Private vNombre As String
+    Private vServicio As String
+    Private vTipoReserva As String
+    Private vPrecioUnit As Double
+    Private vTipoFactura As String
+    Private vMozoID As Integer
+    Private vIngreso As Boolean
+    Private vImprimioT As Integer
+    Private vObservaciones As String
+    Private vTV As Integer
+    Private vTI As Integer
+    Private vClaseTango As Boolean
+    Private vPago As Double
+
+    Private vPrePago As Double
+    Private RowFound() As DataRow
+
+    Public Sub Acreditar()
+        '        Dim filaAcre As DataRow
+        '       filaAcre = odtReserva.Select(IDReserva = vIDreserva)
+        '   RowFound(0)("PrecioUnit")
+        RowFound(0)("ImprimioToken") = vImprimioT + 1
+        RowFound(0)("FechaImp") = oBase.TomarFechaHora
+        '  RowFound(0)("Ingreso") = True
+        Me.GuardarCambios()
+    End Sub
+    Public Sub Ingresar()
+        If Not (IsDBNull(RowFound(0)("ImprimioToken"))) Then
+            If RowFound(0)("ImprimioToken") > 0 Then
+                RowFound(0)("Ingreso") = True
+                RowFound(0)("FechaIngreso") = oBase.TomarFechaHora
+                Me.GuardarCambios()
+                Me.SincronizarBase()
+            End If
+        Else
+            MsgBox("Reserva.Ingresar: No puede ingresar una reserva que no se acredito, primero acreditar luego Ingresar")
+        End If
+    End Sub
+    Public Property ImprimioToken() As Integer
+        Get
+            Return vImprimioT
+        End Get
+        Set(ByVal value As Integer)
+
+        End Set
+    End Property
+    Public Property Ingreso() As Boolean
+        Get
+            Return vIngreso
+        End Get
+        Set(ByVal value As Boolean)
+
+        End Set
+    End Property
+    Public Property ClaseTango() As Boolean
+        Get
+            Return vClaseTango
+        End Get
+        Set(ByVal value As Boolean)
+            RowFound(0)("ClaseTango") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+    Public Property MozoID() As Integer
+        Get
+            Return vMozoID
+        End Get
+        Set(ByVal value As Integer)
+
+        End Set
+    End Property
+
+    Public Property TipoFactura() As String
+        Get
+            Return vTipoFactura
+        End Get
+        Set(ByVal value As String)
+            RowFound(0)("TipoFactura") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Property PrecioUnitario() As Double
+        Get
+            Return vPrecioUnit
+        End Get
+        Set(ByVal value As Double)
+            RowFound(0)("preciounit") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Property Pago() As Double
+        Get
+            Return vPago
+        End Get
+        Set(ByVal value As Double)
+            RowFound(0)("pago") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Property TipoReserva() As String
+        Get
+            Return vTipoReserva
+        End Get
+        Set(ByVal value As String)
+            RowFound(0)("TipoReserva") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Property Servicio() As String
+        Get
+            Return vServicio
+        End Get
+        Set(ByVal value As String)
+            RowFound(0)("Servicio") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+
+    Public Property NombreReserva() As String
+        Get
+            Return vNombre
+        End Get
+        Set(ByVal value As String)
+
+        End Set
+    End Property
+
+    Public Property Observaciones() As String
+        Get
+            Return vObservaciones
+        End Get
+        Set(ByVal value As String)
+            RowFound(0)("observaciones") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+    Private Sub GetReserva()
+
+    End Sub
+
+    Public ReadOnly Property IDReserva() As Integer
+        Get
+            Return vIDreserva
+        End Get
+
+    End Property
+
+    Public Property Mesa() As Integer
+        Get
+            Return vMesa
+        End Get
+        Set(ByVal value As Integer)
+            RowFound(0)("mesa") = value
+            '     vMesa = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+    Public Property CantidadPax() As Integer
+        Get
+            Return vCantPax
+        End Get
+        Set(ByVal value As Integer)
+            RowFound(0)("cantpax") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Property TransferVuelta() As Integer
+        Get
+            Return vTV
+        End Get
+        Set(ByVal value As Integer)
+            RowFound(0)("TransferV") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Property TransferIda() As Integer
+        Get
+            Return vTI
+        End Get
+        Set(ByVal value As Integer)
+            RowFound(0)("Transferi") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Property Prepago() As Integer
+        Get
+            Return vPrePago
+        End Get
+        Set(ByVal value As Integer)
+            RowFound(0)("Prepago") = value
+            Me.GuardarCambios()
+        End Set
+    End Property
+
+    Public Sub Crear(ByVal IDReserva As Integer, ByVal CantidadPax As Short, ByVal Mesa As Short, ByVal PrecioUnit As Integer, ByVal MozoID As Integer, ByVal Notas As String, ByVal NombreReserva As String, ByVal Servicio As String, ByVal TipoReserva As String, ByVal ModoPago As String, ByVal Pagado As Integer, ByVal TransferID As Integer)
+
+    End Sub
+
+    Public Sub Pagar(ByVal IDReserva As Integer, ByVal Cantidad As Integer, ByVal Moneda As String)
+
+    End Sub
+
+    Public Sub Eliminar(ByVal IDReserva As Integer)
+
+    End Sub
+
+    Public Sub AsignarMozo(ByVal IDReserva As Integer, ByVal MozoID As Integer)
+
+    End Sub
+
+    Public Sub TotalDeuda(ByVal IDReserva As Integer)
+
+    End Sub
+
+    Public Sub Upgradear(ByVal Upgrade As String, ByVal IDReserva As Integer)
+
+    End Sub
+
+    Public Sub DarPropina(ByVal IDReserva As Integer, ByVal IDMozo As Integer)
+
+    End Sub
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub New(ByVal FechaCena As Date, ByVal idreserva As Integer)
+        ' oBase.Conectar(oCfg.DevolverValor("base", "acreditador.xml"), oCfg.DevolverValor("server", "acreditador.xml"), oCfg.DevolverValor("ssi", "acreditador.xml"))
+        'odtReserva = oBase.TomarDatos("select * from reservas where idreserva=" & idreserva & "and fecha=" & FechaCena)
+    End Sub
+
+    Public Sub New(ByVal aidreserva As Integer)
+        '    oBase.Conectar(oCfg.DevolverValor("base", "acreditador.xml"), oCfg.DevolverValor("server", "acreditador.xml"), oCfg.DevolverValor("ssi", "acreditador.xml"))
+        '   odtReserva = oBase.TomarDatos("select * from reservas where idreserva=" & idreserva)
+
+        'BUSCO EN EL DT QUE YA TENGO CARGADO LA ROW QUE TIENE EL ID DE RESERVA Y POPULATE LA CLASE RESRVA
+        Try
+            Dim consulta As String
+            consulta = "IDReserva =" & aidreserva
+
+            '          Dim rowfound() As DataRow
+            rowfound = odtReserva.Select(consulta)
+            If (rowfound.Count = 0) Then
+                MsgBox("class reserva: no encontro la reserva segun el id, aca deria llegar siempre un ID, algo anda mal EAF")
+                Return
+            End If
+
+            vIDreserva = aidreserva
+            vNombre = If(IsDBNull(rowfound(0)("Nombre")), 0, rowfound(0)("Nombre"))
+            vMesa = If(IsDBNull(rowfound(0)("mesa")), 0, rowfound(0)("mesa")) 'devuelve un valor solo el qry asi que es un 
+            vCantPax = If(IsDBNull(rowfound(0)("CantPax")), 0, rowfound(0)("CantPax"))
+            vPrecioUnit = RowFound(0)("PrecioUnit")
+            vPrePago = RowFound(0)("PrePago")
+            vPago = If(IsDBNull(RowFound(0)("pago")), 0, RowFound(0)("pago"))
+            vServicio = RowFound(0)("Servicio")
+            vTipoFactura = rowfound(0)("TipoFactura")
+            vTipoReserva = rowfound(0)("TipoReserva")
+            vObservaciones = rowfound(0)("Observaciones")
+            vMozoID = If(IsDBNull(rowfound(0)("MozoID")), 0, rowfound(0)("MozoID"))
+            vIngreso = If(IsDBNull(rowfound(0)("Ingreso")), False, rowfound(0)("Ingreso"))
+            vImprimioT = If(IsDBNull(rowfound(0)("ImprimioToken")), 0, rowfound(0)("ImprimioToken"))
+            vTV = If(IsDBNull(RowFound(0)("TransferV")), 0, RowFound(0)("TransferV"))
+            vTI = If(IsDBNull(RowFound(0)("TransferI")), 0, RowFound(0)("TransferI"))
+            vClaseTango = If(IsDBNull(RowFound(0)("clasetango")), False, RowFound(0)("clasetango"))
+
+        Catch ex As Exception
+            MsgBox("Class Reserva: " & IDReserva & "->>" & ex.Message)
+
+        End Try
+        'odtReserva.Rows(0)("mesa") 'devuelve un valor solo el qry asi que es un 
+
+    End Sub
+
+
+
+End Class
---
+++ BIZL/Reservas.vb
@@ -0,0 +1,185 @@
+﻿Imports Microsoft.SqlServer
+Imports ACRE.Datos
+Imports System.Data
+
+
+Public Class Reservas
+    'heredo las funciones de las colleciones de net
+    Inherits System.Collections.CollectionBase
+    'instancio 1 vez sola la base y el file de config
+    Public Shared oCfg As New Datos.Configuracion
+    Public Shared oBase As New Datos.BaseSQL
+    Public Shared odtReserva As New DataTable
+    Private vFechaCena As Date
+    Private vOrderByField As String
+    Public Function AgregarWalkIn(ByVal aNombre As String, ByVal aServicio As String, ByVal atipoReserva As String, ByVal aMesa As Integer, ByVal aCantidad As Integer, ByVal aPrecioUnit As Double, ByVal aTipoFact As String, ByVal aObservaciones As String) As Integer
+        Dim RowNew As DataRow
+        Dim IDcalcWI As Integer
+
+        Try
+
+            LoadReservas(vFechaCena)
+            'recargo de nuevo el dt de reservas asi me aseguro que me da el ultimo ID
+            RowNew = odtReserva.NewRow 'creo la row que luego agregare al dt
+            ' IDcalcWI = 7000000 + CInt(odtReserva.Rows(odtReserva.Rows.Count / 2).Item("IDReserva")) + odtReserva.Rows.Count + 1 'este ID sera autogenerado, no se por las dudas pongo el idactual + 7000000 + la cantidad de rows + 1 , asi no tengo chance de repetir el IDrESERVA y que guarde, agarro un nro del medio de la lista y le sumo la cuenta esa asi tengo menos chance de duplicar
+
+            'saco el id nuevo ,busco de no duplicar la reserva y que sea un nro por arriba de 7m
+            'defino que todos los WIs seran por arriba del ID 7millon
+            Dim IDmasAlto As Integer = 0
+            For Each fila In odtReserva.Rows 'recorro las reservas abiertas
+                If fila("IDReserva") > IDmasAlto Then ' busco id mas alto
+                    IDmasAlto = fila("IDReserva")
+                    IDcalcWI = IDmasAlto + 1 ' seteo el id con con el id mas alto
+                    If IDmasAlto < 7000000 Then 'si no hay ningun WI antes crea uno nuevo
+                        IDcalcWI = IDmasAlto + 7000000 + 1
+                    End If
+                End If
+
+            Next
+
+            'Dim consulta As String
+            'Dim rowdup() As DataRow
+            'consulta = "IDReserva =" & IDcalcWI
+            'rowdup = odtReserva.Select(consulta)
+            'If (rowdup.Count > 0) Then
+            '    MsgBox("mismo ID")
+            '    IDcalcWI = IDcalcWI + 1
+
+            'End If
+            'fin sacar ID
+
+            RowNew("IDReserva") = IDcalcWI '7000000 + CInt(odtReserva.Rows(0).Item("IDReserva")) + odtReserva.Rows.Count + 1 'este ID sera autogenerado, no se por las dudas pongo el idactual + 7000000 + la cantidad de rows + 1 , asi no tengo chance de repetir el IDrESERVA y que guarde
+            RowNew("Fecha") = vFechaCena
+            RowNew("Nombre") = aNombre
+            RowNew("Servicio") = aServicio
+            RowNew("TipoReserva") = atipoReserva
+            RowNew("Mesa") = aMesa
+            RowNew("CantPax") = aCantidad
+            RowNew("PrecioUnit") = aPrecioUnit
+            RowNew("TipoFactura") = aTipoFact
+            RowNew("Observaciones") = aObservaciones
+            RowNew("ImprimioToken") = 1
+            RowNew("FechaImp") = oBase.TomarFechaHora
+            RowNew("preciounit") = 1
+            RowNew("prepago") = 0
+
+            '        RowNew("Nombre") =
+            odtReserva.Rows.Add(RowNew)
+            'odtReserva.AcceptChanges()
+            Me.GuardarCambios() 'guardo los cambios en la base
+            '  odtReserva.Dispose()
+            LoadReservas(vFechaCena) ' recargo el dt de nuevo, no se si sirve
+        Catch ex As Exception
+            MsgBox("error al agregar reserva " & IDcalcWI & " - " & ex.Message)
+        End Try
+        Return IDcalcWI
+    End Function
+    ReadOnly Property DTReserva() As DataTable
+        Get
+            Return odtReserva
+        End Get
+    End Property
+
+    'Public Function Listar(ByVal QryDT As String) As DataRow
+
+    'End Function
+
+    Public Sub GuardarCambios()
+        oBase.UpdateDatosBindeados()
+    End Sub
+
+    Public Sub Add(ByVal areserva As Reserva)
+        ' List.Add(areserva)
+
+    End Sub
+    Public Sub Remove(ByVal index As Integer)
+        ' Check to see if there is a widget at the supplied index.
+        If index > Count - 1 Or index < 0 Then
+            ' If no widget exists, a messagebox is shown and the operation is 
+            ' cancelled.
+            MsgBox("Index not valid!")
+        Else
+            ' Invokes the RemoveAt method of the List object.
+            List.RemoveAt(index)
+        End If
+    End Sub
+
+    Public ReadOnly Property Item(ByVal idreserva As Integer) As Reserva
+        Get
+            ' The appropriate item is retrieved from the List object and 
+            ' explicitly cast to the Widget type, then returned to the 
+            ' caller.
+            Dim res As New Reserva(idreserva)
+            Return res
+
+            '    Return CType(List.Item(index), Reserva)
+        End Get
+    End Property
+
+    Public Sub New()
+        ' oBase.Conectar(oCfg.DevolverValor("base", "acreditador.xml"), oCfg.DevolverValor("server", "acreditador.xml"), oCfg.DevolverValor("ssi", "acreditador.xml"))
+        ' odtReserva = oBase.TomarDatos("select * from reservas")
+    End Sub
+
+    Public Sub New(ByVal aFechaCena As Date) 'esto deberia ser para que devulva listado
+        LoadReservas(aFechaCena)
+    End Sub
+    Public Sub New(ByVal aFechaCena As Date, ByVal aOrderBYfield As String) 'esto deberia ser para que devulva listado
+        LoadReservas(aFechaCena, aOrderBYfield)
+    End Sub
+    Public Sub New(ByVal aFechaCena As Date, ByVal aWhere As String, ByVal aOrderBYfield As String) 'esto deberia ser para que devulva listado
+        LoadReservas(aFechaCena, aWhere, aOrderBYfield)
+    End Sub
+    Public Sub LoadReservas(ByVal aFechaCenaL As DateTime)
+        Try
+            odtReserva.Clear()
+            vFechaCena = aFechaCenaL
+            oBase.Conectar(oCfg.DevolverValor("base", "acreditador.xml"), oCfg.DevolverValor("server", "acreditador.xml"), oCfg.DevolverValor("ssi", "acreditador.xml"))
+            '        Dim qRy As String = "select * FROM Reservas WHERE (DATEDIFF(day, Fecha,'" & aFechaCenaL & "') = 0) ORDER BY nombre"
+            Dim qRy As String = "select * FROM Reservas WHERE (DATEDIFF(day, Fecha,'" & FormatDateTime(aFechaCenaL, DateFormat.ShortDate) & "') = 0) ORDER BY nombre"
+
+            odtReserva = oBase.TomarDatos(qRy)
+            '       odtReserva = (oBase.TomarDatos("select * FROM Reservas order by IDreserva desc"))
+        Catch ex As Exception
+            MsgBox("Error en Reservas.LoadReservas - " & ex.Message)
+        End Try
+
+    End Sub
+    Public Sub LoadReservas(ByVal aFechaCenaL As Date, ByVal aOrderBYfield As String)
+        Try
+            odtReserva.Clear()
+            vFechaCena = aFechaCenaL
+            vOrderByField = aOrderBYfield
+            oBase.Conectar(oCfg.DevolverValor("base", "acreditador.xml"), oCfg.DevolverValor("server", "acreditador.xml"), oCfg.DevolverValor("ssi", "acreditador.xml"))
+            Dim qRy As String = "select * FROM Reservas WHERE (DATEDIFF(day, Fecha,'" & aFechaCenaL & "') = 0) ORDER BY " & aOrderBYfield
+            odtReserva = oBase.TomarDatos(qRy)
+
+
+        Catch ex As Exception
+            MsgBox("Error en Reservas.LoadReservas - " & ex.Message)
+        End Try
+        '       odtReserva = (oBase.TomarDatos("select * FROM Reservas order by IDreserva desc"))
+    End Sub
+    Public Sub LoadReservas(ByVal aFechaCenaL As Date, ByVal aWhereClause As String, ByVal aOrderBYfield As String)
+        Try
+
+            odtReserva.Clear()
+            vFechaCena = aFechaCenaL
+            vOrderByField = aOrderBYfield
+            oBase.Conectar(oCfg.DevolverValor("base", "acreditador.xml"), oCfg.DevolverValor("server", "acreditador.xml"), oCfg.DevolverValor("ssi", "acreditador.xml"))
+            Dim qRy As String = "select * FROM Reservas WHERE " & aWhereClause & " AND (DATEDIFF(day, Fecha,'" & aFechaCenaL & "') = 0) ORDER BY " & aOrderBYfield
+            odtReserva = oBase.TomarDatos(qRy)
+
+            '       odtReserva = (oBase.TomarDatos("select * FROM Reservas order by IDreserva desc"))
+
+        Catch ex As Exception
+            MsgBox("Error en Reservas.LoadReservas - " & ex.Message)
+        End Try
+
+
+    End Sub
+    Public Sub SincronizarBase()
+        oBase.RefrescarBase()
+
+    End Sub
+End Class
